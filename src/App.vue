40a07777ae5ebb07d5c762a26c4da176983149cd
dannik27 <nikifordan@gmail.com>
2018-12-01 23:19:50 +0300

    get/post works

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <person-card v-bind:key="person.id" v-for="person in persons" v-bind:pers="person" />
+
+    <div>
+      <input v-model="newPerson.name" type="text" />
+      <input v-model="newPerson.age" type="text" />
+      <button v-on:click="save" >Save</button>
+    </div>
+
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import axios from 'axios';
+
+import PersonCard from "./components/person-card";
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+      PersonCard,
+  },
+  data () {
+    return {
+        persons : [],
+        newPerson : {
+            "id" : 0,
+            "name" : "",
+            "age" : 0
+        }
+    }
+  },
+  methods : {
+
+      update : function() {
+          axios
+              .get('http://localhost:3000/')
+              .then(response => (this.persons = response.data));
+      },
+      save : function () {
+
+          axios.post('http://localhost:3000/', this.newPerson)
+              .then((response) => {
+                  this.update()
+              })
+              .catch((error) => {
+                  console.log(error);
+              });
+      }
+
+  },
+  mounted() {
+      this.update();
   }
 }
 </script>
---
+++ src/components/person-card.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card">
+    <h1>{{ pers.name }}</h1>
+    <h2>age: {{ pers.age}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PersonCard',
+  props: {
+    pers: {"id":0, "name":"", "age": 0}
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.card {
+  background: #fff;
+  border-radius: 2px;
+  display: inline-block;
+  margin: 1rem;
+  position: relative;
+  width: 300px;
+  box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+}
+
+</style>
